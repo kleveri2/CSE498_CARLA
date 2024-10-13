b854f0aa344fcb454a06f9cb9c44855e2fc357b8
Gabriel Militello <milite20@msu.edu>
2024-10-13 19:40:38 -0400

    Completed automation of maps to unreal enviornments based on road density and area.  test2 does not work, test 3 does.

--- Unreal/CarlaUE4/Content/CSE498/Levels/Prep_Maps/Find_By_Density.py
+++ Unreal/CarlaUE4/Content/CSE498/Levels/Prep_Maps/Find_By_Density.py
@@ -1,10 +1,13 @@
 import osmnx as ox
 import matplotlib.pyplot as plt
+import requests
 from geopy.distance import geodesic
 from geopy import Point
 import random
 import warnings
 import Add_Signs
+import Convert_and_Display
+import subprocess
 
 def bounding_box(lat, lon, side_km):
         center = Point(lat, lon)
@@ -37,7 +40,7 @@ def find_locations(road_density, area_in_km, samples = 100, max_retries = 5):
 
                 try:
                     bbox = (north_lat, south_lat, east_lon, west_lon)
-                    G = ox.graph_from_bbox(*bbox, network_type='drive', simplify=True)
+                    G = ox.graph_from_bbox(*bbox, network_type='drive', simplify=False)
 
                     if G is None or len(G.nodes) == 0:
                         raise ValueError("bad")
@@ -63,22 +66,73 @@ def plot_road(G):
     plt.show()
 
 
-def main():
-    road_density_in = float(input("Enter desired road density (km/km^2): "))
-    area_in_km = float(input("Enter desired area (km^2): "))
-    sample_size = int(input("Enter number of attempts: "))
-
-    found_location = find_locations(road_density_in, area_in_km, sample_size)
+def find(road_density, area_in_km, sample_size):
+    if road_density is None:
+        while True:
+            # road_density_in = float(input("Enter desired road density (km/km^2): "))
+            # area_in_km = float(input("Enter desired area (km^2): "))
+            # sample_size = int(input("Enter number of attempts: "))
+            road_density = 20
+            area_in_km = 1
+            sample_size = 50
+            if area_in_km > 5:
+                print("large areas may greatly increase loading times (2km^2 = approx 10 mintues of loading)")
+                if input("Continue? (Y/N) ").lower() == "y":
+                    break
+            else:
+                break
+    else:
+        road_density = float(road_density)
+        area_in_km = float(area_in_km)
+        sample_size = int(sample_size)
+    print("Road density:", road_density)
+    print("searching for qualifying road network...")
+    found_location = find_locations(road_density, area_in_km, sample_size)
 
     if found_location:
         lat, lon, road_density, G = found_location
         print("fLatitude: {lat:.6f}, Longitude: {lon:.6f}, Road Density: {rd:.2f} km/km²")
-        plot_road(G)
-        Add_Signs.Add_Signs(G)
+        # plot_road(G)
+        print("Adding signs to map")
+        # Add_Signs.Add_Signs(G)
+        side_km = area_in_km ** 0.5
+        print("calculating bounding box")
+        # bbox = bounding_box(lat, lon, side_km)
+        north_lat, south_lat, east_lon, west_lon = bounding_box(lat, lon, side_km)
+        print("fetching map")
+        overpass_url = "http://overpass-api.de/api/map?bbox="
+        search = str(west_lon) + "," + str(south_lat) + "," + str(east_lon) + "," + str(north_lat)
+        response = requests.get(overpass_url + search)
+        osm_file = 'map.osm'
 
+        print("writing map: ", overpass_url+search)
+        with open(osm_file, 'wb') as f:
+            f.write(response.content)
+        # print("Generate map? \n (Y/n): ")
+        # if input().lower() == 'y':
+        #     print("Generating map...")
+        #
+        #     print("Done!")
+        # else:
+        #     print("Exiting")
+        #     exit(0)
+
+        return overpass_url+search
+
+
+        # osm_filepath = 'map.osm'
+        # print("Downloading osm...")
+        # Convert_and_Display.download_osm_data(north_lat, south_lat, east_lon, west_lon, osm_filepath)
+        # xodr_filepath = 'map.xodr'
+        # print("Here's the big one! (converting and displaying map)")
+        # Convert_and_Display.Convert_and_Display(osm_filepath, xodr_filepath, bbox)
+        # Convert_and_Display.Convert_and_Display(bbox)
+        # ox.save_graph_xml(G, filepath="C:/CSE498_CARLA/Build/libosmscout-source/maps/map.osm")
+        # subprocess.run(["./build.sh"], cwd="C:/CSE498_CARLA/Build/libosmscout-source/maps", shell=True)
     else:
         print("No location found")
+        return 0
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
