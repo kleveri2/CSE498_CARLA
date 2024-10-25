01aad2cfd2fc83c65d0559971ea73ff728263b8d
Zaroffe <117775108+Zaroffe@users.noreply.github.com>
2024-10-25 14:01:20 -0400

    mapillary is working, it may have an issue with detecting all the signs, requiring the surveyed area to be broken up into smaller chunks (item/feature limit is set at 2000 per query on their side).

--- Unreal/CarlaUE4/Content/CSE498/Levels/Prep_Maps/Find_By_Density.py
+++ Unreal/CarlaUE4/Content/CSE498/Levels/Prep_Maps/Find_By_Density.py
@@ -5,7 +5,8 @@ from geopy.distance import geodesic
 from geopy import Point
 import random
 import warnings
-import Add_Signs
+# import Add_Signs
+import Add_Signs_Mapillary
 def bounding_box(lat, lon, side_km):
     center = Point(lat, lon)
     d = side_km / 2.0
@@ -52,18 +53,54 @@ def plot_road(G):
     plt.title('Road Network of Found Location')
     plt.show()
 
+def plot_signs(G, signs):
+    # Plot the graph without projecting it (coordinates remain in lat/lon)
+    fig, ax = ox.plot_graph(
+        G,
+        show=False,
+        close=False,
+        dpi=200,
+        node_size=0,
+        edge_color='gray',
+        edge_linewidth=0.5,
+        bgcolor='white'
+    )
+
+    # Extract longitude and latitude from the signs data
+    signs_coords = [
+        (feature['geometry']['coordinates'][0], feature['geometry']['coordinates'][1])
+        for feature in signs
+    ]
+
+    # Unzip the coordinates into separate lists
+    lons, lats = zip(*signs_coords) if signs_coords else ([], [])
+
+    # Plot the signs on the map
+    ax.scatter(lons, lats, s=30, c='red', marker='o', label='Stop Sign', zorder=5)
+
+    plt.title('Road Network with Sign Locations')
+    plt.legend()
+    plt.show()
+
 def find(road_density=20, area_in_km=1, sample_size=50):
     found_location = find_locations(road_density, area_in_km, sample_size)
     if found_location:
         lat, lon, road_density, G = found_location
-        print("Plotting Map")
+        # print("Plotting Map")
         plot_road(G)
 
         side_km = area_in_km ** 0.5
         north_lat, south_lat, east_lon, west_lon = bounding_box(lat, lon, side_km)
 
-        Add_Signs.Add_Signs(G, north_lat, south_lat, east_lon, west_lon)
-
+        print("Finding stop signs")
+        # Add_Signs.Add_Signs(G, north_lat, south_lat, east_lon, west_lon)
+        # signs = Add_Signs.generate_synthetic_signs(G)
+        signs = Add_Signs_Mapillary.fetch_features(south_lat, west_lon, north_lat, east_lon)
+        if len(signs) > 0:
+            print(signs)
+            plot_signs(G, signs)
+        else:
+            print("No Stop signs")
         print("fetching map")
         overpass_url = "http://overpass-api.de/api/map?bbox="
         search = str(west_lon) + "," + str(south_lat) + "," + str(east_lon) + "," + str(north_lat)
